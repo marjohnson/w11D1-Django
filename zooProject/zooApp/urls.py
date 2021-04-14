e10f36bef28283ac60b02bce3799f3d9e0b10061
dojo24 <mlongenberger@codingdojo.com>
2021-04-14 17:16:00 -0400

    removed name= line in app urls.py as it will fail when adding html tonight

--- zooProject/zooApp/urls.py
+++ zooProject/zooApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/',views.about, name='about'),
-    path('contributors/', views.contributors, name='contributors')
+    path('', views.index),
+    path('about/',views.about),
+    path('contributors/', views.contributors)
 ]
