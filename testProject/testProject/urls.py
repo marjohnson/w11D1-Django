e94dcdd6cde457ef0576215f4569da5a4858f4c5
dojo24 <mlongenberger@codingdojo.com>
2021-04-19 15:47:05 -0400

    adding missing content

--- testProject/testProject/urls.py
+++ testProject/testProject/urls.py
@@ -18,6 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('testApp.urls')),
-    # path('about/', include('testApp.urls')),
     path('admin/', admin.site.urls),
 ]
