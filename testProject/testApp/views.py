d4f8b3671c76ab18707cdf659b08a2f313640250
dojo24 <mlongenberger@codingdojo.com>
2021-04-19 16:14:14 -0400

    tested adding a form

--- testProject/testApp/views.py
+++ testProject/testApp/views.py
@@ -5,3 +5,12 @@ def index(request):
 
 def about(request):
     return HttpResponse("About Melissa")
+
+def testForm(request):
+    if request.method == "GET":
+        print("A GET request was made from this route")
+        print(request.GET)
+        return render(request, "testForm.html")
+    if request.method == "POST":
+        print("A POST request was made from this route")
+        print(request.POST)
