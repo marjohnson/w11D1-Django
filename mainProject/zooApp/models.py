abd51f0f657c5213a1220fce0f9a8fdcaa9de559
dojo24 <mlongenberger@codingdojo.com>
2021-05-03 17:04:00 -0400

    saving test

--- mainProject/zooApp/models.py
+++ mainProject/zooApp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Members(models.Model):
+    memberName = models.CharField(max_length=255)
+    addDate = models.DateTimeField(auto_now_add=True)
+    updateDate = models.DateTimeField(auto_now=True)
